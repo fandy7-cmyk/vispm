21451d9ee498234a78eec50e1826dc0027bbe67e
Fandy Burhan <f74262944@gmail.com>
2026-03-01 18:11:53 +0800

    Update drive-delete.js

--- netlify/functions/drive-delete.js
+++ netlify/functions/drive-delete.js
@@ -5,7 +5,7 @@ async function getAuthClient() {
   const auth = new GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      private_key: Buffer.from(process.env.GOOGLE_PRIVATE_KEY_BASE64 || '', 'base64').toString('utf8'),
     },
     scopes: ['https://www.googleapis.com/auth/drive'],
   });
